b95872ce5e12c33b08d78a53b360a11ad2ff5cb1
Arvind <arvind241190@gmail.com>
2022-05-05 14:37:15 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from "react";
+// import axios from 'axios';
+// import style from './App.module.css'
+// const App=()=> {
+//   const[data,setdata]=useState([]);
+//  // const[item,setItem]=useState([]);
+//   const handleClick=()=>{
+//     axios.get("https://fakestoreapi.com/products")
+//   .then((resp)=>{
+//     //console.log(resp);
+//     //const cate=resp.data[0].category;
+//     //const img=resp.data[0].image;
+//     const des=resp.data;
+//     setdata(des);
+//     console.log('clicked');
 
-function App() {
+//   })
+//   .catch((error)=>{
+//     console.log(error);
+//   })
+//   }
+//   return (
+   
+//    <div className={style.bg}>
+//      <h1>Fake Store API</h1>
+//      <input type="button" className={style.btn1} value="Get Data" onClick={handleClick} />
+    
+//      <hr className={style.hr}/>
+     
+//      <div className={`${style.set} ${style.container}`}>
+//        {
+//        data.map((d)=>{
+//          return(
+          
+//            <div className={style.box} key={d.id}>            
+//              <h2>{d.title}</h2>
+//              <p>{d.description}</p> <br/>            
+//              <p className={style.price}>Price :&#8377; {d.price}</p><br/>
+//              <div >
+//              <img className={style.img} src={d.image} alt="" />
+//              </div>
+//            </div>
+          
+//          );
+//        })
+//      }
+//      </div>
+    
+     
+//    </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import FetchNews from './component/FetchNews';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-secondary' style={{opacity : "1"}}>
+      <div className='container text-center py-5 '>
+        <h1 >Daily News Headline</h1>
+        <FetchNews/>
+    </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
---
+++ src/component/FetchNews.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function FetchNews() {
+    const [news, setNews] = useState([]);
+    const fetchNews = (value) => {
+        axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=f6141f13082342d6a2d4c6321684198b')
+            .then((response) => {
+                //console.log(response);
+                setNews(response.data.articles)
+                console.log(response.data.articles);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    useEffect(() => {
+        fetchNews();
+    });
+
+    return (
+
+        <>
+            <button type="button" className='btn btn-primary d-none' value="" onClick={fetchNews} >Fetch News</button>
+
+
+            <div className="container ">
+                <div className="row">
+                    {
+                        news.map((e,i) => {
+                            return (
+                                <div className="col-4 my-5" >
+                                    <div class="card h-100"  >
+                                    <h2 className="card-title">{e.title }</h2>
+                                        <img src={e.urlToImage} className='card-img-top' alt="..." width="100%" height="auto" />
+                                        <div className="card-body">
+                                            
+                                            <p >{e.description}</p>
+                                            <a href={e.url} target="_blank"  className="btn btn-primary text-center">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+
+        </>
+
+
+    );
+};
+
+export default FetchNews;
+
+{/*  */ }
+
